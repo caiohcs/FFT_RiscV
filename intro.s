0779d3a7442e7c388af340aef4880d485a48458b
André Luis <andre.luis.e3@gmail.com>
2020-07-15 11:01:03 -0300

    Add files via upload

--- intro.s
+++ intro.s
@@ -1,30 +1,21 @@
 .globl __start
 
+.rodata
+  Error_Size: .string "Vector Size is not a power of two!"
+
 .data
 
-  X: .byte 1,2,3,4,5,6,7,8,'$'
-  X_even: .zero 5
-  X_odd:
+  X: .float 1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0
+  X_size: .byte 8
   
-.rodata
-  Error_Size: .string "Vector Size is not a power of two!" 
+  X_even: .zero 16 # 5 Positions (32*4)/8
+  X_odd: .zero 16 # 5 Positions (32*4)/8
   
 .text
-
-CheckSize:
-  la a1,X
-  li a2,1
-  li a3,36 # Dollar Sign to Compare
-  CheckSize_Loop:
-  lb a4,0(a1)
-  beq a4,a3, CheckSize_Result  # Compate to Dollar Sign
-  addi a1,a1,1
-  addi a2,a2,1
-  jal x0,CheckSize_Loop
   
-  CheckSize_Result:
+CheckSize:
+  lb a2,X_size
   li a3,1
-  sub a2,a2,a3 # Subtract Dollar Sign from Size
   sub a3,a2,a3
   and a3,a2,a3
   beq a3,x0,SplitArrays
@@ -34,20 +25,22 @@ CheckSize:
   ebreak 
   
 SplitArrays:
-  la a1,X
+  la a1, X
   la a2, X_odd
-  la a3,X_even
-  li a6,36 # Dollar Sign to Compare
+  la a3, X_even
+  lb a6, X_size
+  addi a6,a6,1 # Array Size + 1 to Compare
+  li a7, 2 # Position Flag
   SplitArrays_Loop:
-  lb a4,0(a1)
-  lb a5,1(a1)
-  beq a4,a6,Return
-  beq a5,a6,Return # Compare to Dollar Sign
-  sb a4,0(a2)
-  sb a5,0(a3)
-  addi a1,a1,2
-  addi a2,a2,1
-  addi a3,a3,1
+  bge a7,a6,Return
+  flw f1,0(a1)
+  flw f2,4(a1)
+  fsw f1,0(a2)
+  fsw f2,0(a3)
+  addi a1,a1,8 # Jump Two Positions (32*2)/8
+  addi a2,a2,4 # Jump One Position (32*1)/8
+  addi a3,a3,4 # Jump One Position (32*1)/8
+  addi a7,a7,2
   jal x0,SplitArrays_Loop
   
   Return:
